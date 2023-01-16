df9e6bb22ce31af54cf5a26ecc2249876960513a
brunomont <brunoadrian.mont@gmail.com>
2023-01-16 15:33:50 -0500

    add: en LN Categoria, Unidad y Producto

---
+++ CapaNegocios/CategoriaLN.vb
@@ -0,0 +1,15 @@
+﻿Imports CapaAccesoDatos
+Imports CapaEntidad
+Public Class CategoriaLN
+
+    Private ReadOnly _categoriaAD As CategoriaAD
+
+    Public Sub New()
+        _categoriaAD = New CategoriaAD()
+    End Sub
+
+    Public Function ListarCategorias() As List(Of Categoria)
+        Return _categoriaAD.ListarCategorias()
+    End Function
+
+End Class
